95d679e09c6b2ae97d1d7c1d1aa4d00d6fcb46d9
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-06-27 12:35:29 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,16 +8,16 @@ import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 export default function Navbar(){
 return(
 
-    <nav className="navbar navbar-expand-lg navbar custom-navbar">
-    <div className="container-fluid">
-      <Link href="/" className="navbar-brand">
+    <nav className="navbar navbar-expand-lg navbar custom-navbar p-0 m-0">
+    <div className="container-fluid p-0 m-0">
+      <Link href="/" className="navbar-brand p-0 m-0">
         
           <Logo /> {/* Logo à gauche */}
         
       </Link>
       <DarkModeToggle />
      <button
-        className={`navbar-toggler ${styles.customToggler}`}
+        className={`navbar-toggler ${styles.customToggler} p-0 m-0`}
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarNav"
@@ -27,17 +27,17 @@ return(
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className={`${styles.links} `}>
+      <div className={`${styles.links} p-0 m-0`}>
       {/* Toggle de mode sombre (ou autre composant) au centre */}
 
 
       {/* Bouton toggle pour mobile */}
       
 
-      <div className={`collapse navbar-collapse  ${styles.links}`} id="navbarNav">
-        <ul className="navbar-nav">
+      <div className={`collapse navbar-collapse  ${styles.links} p-0 m-0`} id="navbarNav">
+        <ul className="navbar-nav p-0 m-0">
           {links.map(link => (
-            <li className="nav-item" key={link.id}>
+            <li className="nav-item p-0 m-0" key={link.id}>
               <Link href={link.url} className={`nav-link ${styles.link}`}>
               {link.title}
               </Link>
