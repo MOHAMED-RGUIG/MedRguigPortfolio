a66803170f7a69e90b920dfe9f6c852790513d46
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-07-03 16:14:41 +0200

    Update data.js

--- src/app/(dynamic)/portfolio/data.js
+++ src/app/(dynamic)/portfolio/data.js
@@ -1,20 +1,20 @@
 export const portfolios = [
     {   id: 1,
-        title:"Lavazza Platform 'Web Mobile App'",
+        title:"Lavazza Platform Web Mobile App",
         image:"https://i.postimg.cc/Dw6NgqNS/2.png",
         description:"Login:'johnson@example.com',</br>password:'password'.</br>Tech: React & Redux & MSSQL & CVS & Node+Express",
         url:"https://lavazzaplatform14.vercel.app/"
     },
     {
         id : 2,
-        title:"Lavazza Mockup 'Web Mobile App'",
+        title:"Lavazza Mockup Web Mobile App",
         image:"https://i.postimg.cc/zXn6LmSS/1.png",
         description:"Login:'A.khaoulani@topclass.ma',</br>password:'Top_2024'.</br>Tech: Mern & Redux",
         url:"https://lavazzamockup15.vercel.app/"
     },
     {
         id : 3,
-        title:"Coffee Shop 'Web App'",
+        title:"Coffee Shop Web App",
         image:"https://i.postimg.cc/W4fHRsTj/3.png",
         description:"Tech: Next.js & MoongoDB & api(Mockup json file + products json file on github)",
         url:"https://coffee-shop3-phi.vercel.app/"
@@ -23,7 +23,7 @@ export const portfolios = [
 
     {
         id : 4,
-        title:"Mapping-Tech 'Web Site'",
+        title:"Mapping-Tech Web Site",
         image:"https://i.postimg.cc/xCJFnB1m/4.png",
         description:"Tech: Html,Css,JS,Bootstrap",
         url:"https://mohamed-rguig.github.io/MappingTechnologies-rguig/"
@@ -37,4 +37,12 @@ export const portfolios = [
         url:"https://mohamed-rguig.github.io/LavazzaLandingPage/"
     
     },
+    {
+        id : 6,
+        title:"Lavazza Atelier form Web Mobile App(No BackEnd)(Encours de développement).</br>Entrer : DEM2406-0070313 comme N° de la </br> demande de service pour test",
+        image:"https://i.postimg.cc/pT0YFfy9/Atelier-Form.png",
+        description:"React, JS , Redux",
+        url:"https://atleir-form3.vercel.app/"
+    
+    }
     ]
