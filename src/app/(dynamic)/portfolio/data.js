0f4e08a9a3d67258bda8e98955483fdd057c79ed
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-10-31 00:20:06 +0100

    Update data.js

--- src/app/(dynamic)/portfolio/data.js
+++ src/app/(dynamic)/portfolio/data.js
@@ -1,45 +1,45 @@
 export const portfolios = [
-  {   id: 1,
-        title:"Lavazza Gestion des commandes Web Mobile App",
-        image:"https://i.postimg.cc/Fsq1B549/Capture.png",
-        description:"Login:'johnson@example.com',</br>password:'password'.</br>Tech: React & Redux & MSSQL & CVS & Node+Express",
-        url:"https://topclass1.vercel.app/"
-    },
-    {   id: 2,
-         title:"Lavazza Gestion des taches Web Mobile App",
-        image:"https://i.postimg.cc/L52NDWWX/taches.png",
-        description:"Login:'johnson@example.com',</br>password:'password'.</br>Tech: React & VITE & MSSQL & Node+Express",
-        url:"https://topclassgstc.app.genez.io/"
-    
-    },
-    {   id: 3,
-        title:"Poles handassa Web site",
-        image:"https://i.postimg.cc/vZ2RzbFz/Capture1.png",
-        description:"React & Redux & Node",
-        url:"https://chak.vercel.app/"
-    },
-    {
-        id : 4,
-        title:"Mapping-Tech Web Site",
-        image:"https://i.postimg.cc/xCJFnB1m/4.png",
-        description:"Tech: Html,Css,JS,Bootstrap",
-        url:"https://mohamed-rguig.github.io/MappingTechnologies-rguig/"
-    
-    },
-     {
-        id : 5,
-        title:"Lavazza landing page'",
-        image:"https://i.postimg.cc/Tw1kLpn0/5.png",
-        description:"Tech: Html,Css,Bootstrap",
-        url:"https://mohamed-rguig.github.io/LavazzaLandingPage/"
-    
-    },
-    {
-        id : 6,
-        title:"Lavazza Atelier form Web Mobile App",
-        image:"https://i.postimg.cc/cCBgVY8x/Atelier-Form1.png",
-        description:"React, JS , Redux ,(No BackEnd)(Encours de développement).</br>Entrer : DEM2406-0070313 comme N° de la </br> demande de service pour test",
-        url:"https://atleir-form3.vercel.app/"
-    
-    }
-    ]
+    {   id: 1,
+          title:"Lavazza Gestion des commandes Web Mobile App",
+          image:"https://i.postimg.cc/Fsq1B549/Capture.png",
+          description:"Login:'johnson@example.com',</br>password:'password'.</br>Tech: React & Redux & MSSQL & CVS & Node+Express",
+          url:"https://topclass1.vercel.app/"
+      },
+      {   id: 2,
+           title:"Lavazza Gestion des taches Web Mobile App",
+          image:"https://i.postimg.cc/L52NDWWX/taches.png",
+          description:"Login:'johnson@example.com',</br>password:'password'.</br>Tech: React & VITE & MSSQL & Node+Express",
+          url:"https://topclassgstc.app.genez.io/"
+      
+      },
+      {   id: 3,
+          title:"Poles handassa Web site",
+          image:"https://i.postimg.cc/vZ2RzbFz/Capture1.png",
+          description:"React & Redux & Node",
+          url:"https://chak.vercel.app/"
+      },
+      {
+          id : 4,
+          title:"Mapping-Tech Web Site",
+          image:"https://i.postimg.cc/xCJFnB1m/4.png",
+          description:"Tech: Html,Css,JS,Bootstrap",
+          url:"https://mohamed-rguig.github.io/MappingTechnologies-rguig/"
+      
+      },
+       {
+          id : 5,
+          title:"Lavazza landing page'",
+          image:"https://i.postimg.cc/Tw1kLpn0/5.png",
+          description:"Tech: Html,Css,Bootstrap",
+          url:"https://mohamed-rguig.github.io/LavazzaLandingPage/"
+      
+      },
+      {
+          id : 6,
+          title:"Lavazza Atelier form Web Mobile App",
+          image:"https://i.postimg.cc/cCBgVY8x/Atelier-Form1.png",
+          description:"React, JS , Redux ,(No BackEnd)(Encours de développement).</br>Entrer : DEM2406-0070313 comme N° de la </br> demande de service pour test",
+          url:"https://atleir-form3.vercel.app/"
+      
+      }
+      ]
