ded5b94f60785ae90fa056d5e85f51e56a2f4f27
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-10-30 23:32:13 +0100

    Update page.jsx

--- src/app/(static)/about/page.jsx
+++ src/app/(static)/about/page.jsx
@@ -17,10 +17,10 @@ function about() {
 
 <div className={`col-md-12 col-12 mb-5 ${styles.col}`}>
 <h1>About</h1>
-     Après avoir pris la licence en economie & gestion et travailler depuis un certain temps en comptabilité, et lorsqu'un
+     Après avoir pris la licence en economie & gestion et travailler depuis un certain temps en recouvrement, et lorsqu'un
       jour j'ai entendu parlez de domaine de l'informatique et ses dimensions, j'avais
        décider de faire des sacrifices et prendre mon choix difficile pour suivre mes 
-       ambitions et commencer le challenge de devenir un développeur professionel.<br></br>J'occupe maintenant le poste de dévelopeur WEB chez TopClass Lavazza Maroc.
+       ambitions et commencer le challenge de devenir un développeur web professionel.<br></br>J'occupe maintenant le poste de dévelopeur WEB chez TopClass Lavazza Maroc.
        <br></br>Vous
         trouverez ci-dessus la rubrique : mon portfolio, qui montre l'ensemble de mes projets personnel et professionnel durant la période de mon expérience.
   </div></div></div>
